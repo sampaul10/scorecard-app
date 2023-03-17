669052e9f7d742055791aa01bb81178dba31876e
bsimmonds28 <bsimmonds28@gmail.com>
2023-03-17 08:52:49 -0400

    Fixed delete route

--- models/index.js
+++ models/index.js
@@ -2,26 +2,26 @@ const Golfer = require('./Golfer');
 const Game = require('./Game');
 const Hole = require('./Hole');
 
-// Game belongsTo Golfer
-Game.belongsTo(Golfer, {
-  foreignKey: 'golfer_id',
-})
-
 // Golfer hasMany Games
 Golfer.hasMany(Game, {
   foreignKey: 'golfer_id',
   onDelete: 'CASCADE',
 })
 
-// hole belongsTo game
-Hole.belongsTo(Game, {
-  foreignKey: 'game_id',
+// Game belongsTo Golfer
+Game.belongsTo(Golfer, {
+  foreignKey: 'golfer_id',
 })
 
 // Game hasMany hole
 Game.hasMany(Hole, {
   foreignKey: 'game_id',
   onDelete: 'CASCADE',
+})
+
+// hole belongsTo game
+Hole.belongsTo(Game, {
+  foreignKey: 'game_id',
 })
 
 module.exports = {
