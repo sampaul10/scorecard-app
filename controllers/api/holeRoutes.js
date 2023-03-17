5c9ce9442fba02f2bc3aa67f5aa0ab3426c16aef
bsimmonds28 <bsimmonds28@gmail.com>
2023-03-17 12:51:02 -0400

    hole put route parameter

--- controllers/api/holeRoutes.js
+++ controllers/api/holeRoutes.js
@@ -9,27 +9,27 @@ router.post('/', async (req, res) => {
   
       res.status(200).json(newHole);
     } catch (err) {
+      console.log("hole routes post error" + err)
       res.status(400).json(err);
     }
   });
 
   router.put('/:id', async (req, res) => {
-    console.log(req.body);
-    console.log(req.session)
-    console.log(req.body)
+    console.log("params" + req.params.id);
+    console.log("session" + req.session)
       try {
           const newScore = await Hole.update({
             score: req.body.score
           },
           { where: {
-            game_id: req.params.id,
-            hole_number: 7
+            game_id: 1,
+            hole_number: 1
           }
           });
      
         res.status(200).json(newScore);
       } catch (err) {
-        console.log(err)
+        console.log("hole routes put error" + err)
         res.status(400).json(err);
       }
     });
